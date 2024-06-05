cd8f94bb761aa3da720e0c940a33f824cdaa51db
canyalniz <canyalniz97@gmail.com>
2024-06-05 12:18:23 +0200

    Added model checkpoint functionality

--- ModelActions.py
+++ ModelActions.py
@@ -1,4 +1,6 @@
+import datetime
 import torch
+import os
 
 
 def train_for_epochs(
@@ -11,9 +13,20 @@ def train_for_epochs(
     epoch_lr_scheduler=None,
     step_lr_scheduler=None,
     tb_writer=None,
+    save_model_epochs_period=None,
+    skip_epochs_before_saving=0,
+    model_save_dir="model_checkpoints",
 ):
     global_steps = 0
     model.train(True)
+
+    train_start_id = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    if not os.path.exists(model_save_dir):
+        os.makedirs(model_save_dir)
+
+    run_path = os.path.join(model_save_dir, train_start_id)
+    os.makedirs(run_path)
 
     for ep in range(epochs):
         epoch_loss_value = 0
@@ -50,6 +63,12 @@ def train_for_epochs(
 
         if epoch_lr_scheduler:
             epoch_lr_scheduler.step()
+
+        if save_model_epochs_period:
+            if ep > skip_epochs_before_saving and ep % save_model_epochs_period == 0:
+                identifier = f"{train_start_id}_ep{ep}"
+                path = os.path.join(run_path, identifier)
+                torch.save(model, path)
 
         if val_loader:
             val_loss = 0
