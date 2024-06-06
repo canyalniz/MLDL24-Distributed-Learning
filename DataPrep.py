47be2f5099b5d3bdd181e5995476146422d4d2e3
canyalniz <canyalniz97@gmail.com>
2024-06-06 12:01:07 +0200

    Made GPU preloading of the dataset optional, moved away from ToTensor, it will be deprecated.

--- DataPrep.py
+++ DataPrep.py
@@ -2,12 +2,16 @@ import torch
 from torchvision import datasets
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data import random_split
-from torchvision.transforms import ToTensor
+from torchvision.transforms import v2
 
 
 class Cifar100CudaDataset(Dataset):
     def __init__(
-        self, root="Datasets", train=True, download=False, transform=ToTensor()
+        self,
+        root="Datasets",
+        train=True,
+        download=False,
+        transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
     ) -> None:
         cifar100 = datasets.CIFAR100(
             root=root,
@@ -39,6 +43,7 @@ class DataPrepper:
         root="Datasets",
         download=False,
         val_ratio=0,
+        transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
     ) -> None:
         if val_ratio < 0 or val_ratio >= 1:
             raise ValueError("Validation ratio has to be in [0,1)")
@@ -46,20 +51,35 @@ class DataPrepper:
         self.root = root
         self.download = download
         self.val_ratio = val_ratio
+        self.transform = transform
 
-    def get_datasets(self):
-        cifar100_training = Cifar100CudaDataset(
-            root=self.root,
-            train=True,
-            download=self.download,
-            transform=ToTensor(),
-        )
-        cifar100_testing = Cifar100CudaDataset(
-            root=self.root,
-            train=False,
-            download=self.download,
-            transform=ToTensor(),
-        )
+    def get_datasets(self, cuda=True):
+        if cuda:
+            cifar100_training = Cifar100CudaDataset(
+                root=self.root,
+                train=True,
+                download=self.download,
+                transform=self.transform,
+            )
+            cifar100_testing = Cifar100CudaDataset(
+                root=self.root,
+                train=False,
+                download=self.download,
+                transform=self.transform,
+            )
+        else:
+            cifar100_training = datasets.CIFAR100(
+                root=self.root,
+                train=True,
+                download=self.download,
+                transform=self.transform,
+            )
+            cifar100_testing = datasets.CIFAR100(
+                root=self.root,
+                train=False,
+                download=self.download,
+                transform=self.transform,
+            )
 
         if self.val_ratio == 0:
             return (cifar100_training, cifar100_testing, None)
@@ -71,8 +91,10 @@ class DataPrepper:
         )
         return (training_set, cifar100_testing, val_set)
 
-    def get_dataloaders(self, batch_size=32, val_full_batchsize=False, **kwargs):
-        train_set, test_set, val_set = self.get_datasets()
+    def get_dataloaders(
+        self, cuda=True, batch_size=32, val_full_batchsize=False, **kwargs
+    ):
+        train_set, test_set, val_set = self.get_datasets(cuda)
         if val_set:
             if val_full_batchsize:
                 return (
